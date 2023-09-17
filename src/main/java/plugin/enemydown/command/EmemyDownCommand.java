36f40062faa29c97bdd76dc7ae26d1c3caf9dd2e
ginga <izuto.kudo.cop@gmail.com>
2023-09-18 03:24:33 +0900

    メソッド抽出前

--- src/main/java/plugin/enemydown/command/EmemyDownCommand.java
+++ src/main/java/plugin/enemydown/command/EmemyDownCommand.java
@@ -14,18 +14,19 @@ public class EmemyDownCommand implements CommandExecutor {
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if(sender instanceof Player player) {
+      World world = player.getWorld();
+
       player.setHealth(20);
       player.setFoodLevel(20);
 
-      World world = player.getWorld();
       Location playerLocation = player.getLocation();
       double x = playerLocation.getX();
       double y = playerLocation.getY();
       double z = playerLocation.getZ();
-
       int random = new SplittableRandom().nextInt(15) - 7;
+      Location enemySpawnLocation = new Location(world, (x + random), y , (z + random));
 
-      world.spawnEntity(new Location(world, (x + random), y , (z + random)), EntityType.ZOMBIE);
+      world.spawnEntity(enemySpawnLocation, EntityType.ZOMBIE);
     }
     return false;
   }
